b2a3c6d1ee154a68c0c7002aec808ffd43851792
Anirudh <an516658@dal.ca>
2024-05-02 22:33:31 -0600

    Added maps with markers

--- anyone-can-deliver/src/screens/OrdersScreen/index.js
+++ anyone-can-deliver/src/screens/OrdersScreen/index.js
@@ -1,16 +1,64 @@
-import { useMemo, useRef } from "react";
-import { View, Text } from "react-native";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { View, Text, Dimensions, useWindowDimensions } from "react-native";
 import BottomSheet, { BottomSheetFlatList, BottomSheetView } from '@gorhom/bottom-sheet';
 import orders from '../../../assets/data/orders.json'
 import OrderItem from "../../components/OrderItem";
+import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location';
+import { Entypo } from '@expo/vector-icons'
 
 const OrdersScreen = () => {
 
+    const [locationPermission, setLocationPermission] = useState(false);
     const bottomSheetRef = useRef(null);
+    const { width, height } = useWindowDimensions();
     const snapPoints = useMemo(() => ["12%", "90%"], [])
+
+    useEffect(() => {
+        (async () => {
+            await requestLocationPermission();
+        })();
+    }, []);
+
+    const requestLocationPermission = async () => {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Permission to access location was denied');
+            return;
+        }
+        setLocationPermission(true);
+    };
 
     return (
         <View style={{ backgroundColor: 'lightblue', flex: 1 }}>
+            {
+                locationPermission ? (
+                    <MapView
+                        style={{ height: height, width: width }}
+                        showsUserLocation
+                        followsUserLocation
+                    >
+                        {
+                            orders.map((order) => (
+                                <Marker
+                                    key={order.id}
+                                    title={order.Restaurant.name}
+                                    description={order.Restaurant.address} coordinate={{
+                                        latitude: order.Restaurant.lat,
+                                        longitude: order.Restaurant.lng
+                                    }}
+                                >
+                                    <View style={{ backgroundColor: 'green', padding: 5, borderRadius: 20 }}>
+                                        <Entypo name="shop" size={24} color="white" />
+                                    </View>
+                                </Marker>
+                            ))
+                        }
+
+                    </MapView>
+                ) : (
+                    <Text>Requesting for location permission...</Text>
+                )}
             <BottomSheet
                 ref={bottomSheetRef}
                 index={1}
