6d1b18327777bdc5784c3d6bef849d415741a35a
Anirudh <an516658@dal.ca>
2024-07-16 17:39:26 -0600

    Removed createmenuItem component as redundant

--- anyone-can-kitchen-dashboard/src/components/AppRoutes/index.js
+++ anyone-can-kitchen-dashboard/src/components/AppRoutes/index.js
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "../../contexts/ProtectedRoutes";
+import SignIn from "../../modules/AuthScreens/SignIn";
+import SignUp from "../../modules/AuthScreens/SignUp";
+import DetailedOrder from "../../modules/DetailedOrder";
+import OrderHistory from "../../modules/OrderHistory";
 import Orders from "../../modules/Orders";
 import RestaurantMenu from "../../modules/RestaurantMenu";
-import CreateMenuItem from "../../modules/CreateMenuItem";
-import OrderHistory from "../../modules/OrderHistory";
-import DetailedOrder from "../../modules/DetailedOrder";
 import Settings from "../../modules/Settings";
-import SignUp from "../../modules/AuthScreens/SignUp";
-import SignIn from "../../modules/AuthScreens/SignIn";
-import ProtectedRoute from "../../contexts/ProtectedRoutes";
 
 const AppRoutes = () => {
     return (
@@ -17,7 +16,6 @@ const AppRoutes = () => {
             <Route path="/" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
             <Route path="order/:id" element={<ProtectedRoute><DetailedOrder /></ProtectedRoute>} />
             <Route path="menu" element={<ProtectedRoute><RestaurantMenu /></ProtectedRoute>} />
-            <Route path="menu/create" element={<ProtectedRoute><CreateMenuItem /></ProtectedRoute>} />
             <Route path="order-history" element={<ProtectedRoute><OrderHistory /></ProtectedRoute>} />
             <Route path="settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
         </Routes>
